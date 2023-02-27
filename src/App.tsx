bb343398d8c601e87df49fce47dfc60691cda234
fran <franciscopischetz@gmail.com>
2023-02-27 00:55:41 -0300

    Fix small text details

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div className='container'>
         <img src={QRcode} alt="QR Code" />
         <div className="card-content">
-          <h1>Improve your front-end skills by building a projects</h1>
+          <h1>Improve your front-end skills by building projects</h1>
           <h5>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h5>
         </div>
       </div>
